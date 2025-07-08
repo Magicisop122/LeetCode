6e18dad4f85af57f32effcdcb0a502f61fe45e2c
Magicisop122 <gdivekar122@gmail.com>
2025-07-07 22:58:50 -0400

    Time: 15 ms (13.52%) | Memory: 25.6 MB (5.10%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+
+        def valid(l: int, r: int) -> bool:
+            if l >= r:
+                return True
+
+            while l < r and not s[l].isalnum():
+                l += 1
+            
+            while l < r and not s[r].isalnum():
+                r -= 1
+
+            if l >= r:
+                return True
+
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            return valid(l + 1, r - 1)
+
+        return valid(0, len(s) - 1)
