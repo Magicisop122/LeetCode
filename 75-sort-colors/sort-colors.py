f908a19f8a038ef25c0550bb277b95f503b21e4a
Magicisop122 <gdivekar122@gmail.com>
2025-07-13 18:57:08 -0400

    Time: 0 ms (100.00%) | Memory: 17.9 MB (12.37%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -5,25 +5,46 @@ class Solution:
         """
 
         # better solution than brute force
-        cnt0 = 0
-        cnt1 = 0
-        cnt2 = 0
+        # cnt0 = 0
+        # cnt1 = 0
+        # cnt2 = 0
 
-        for n in nums:
-            if n == 0:
-                cnt0 += 1
-            elif n == 1:
-                cnt1 += 1
+        # for n in nums:
+        #     if n == 0:
+        #         cnt0 += 1
+        #     elif n == 1:
+        #         cnt1 += 1
+        #     else:
+        #         cnt2 += 1
+        
+        # for i in range(cnt0):
+        #     nums[i] = 0
+        
+        # for i in range(cnt0, cnt0 + cnt1):
+        #     nums[i] = 1
+
+        # for i in range(cnt0 + cnt1, len(nums)):
+        #     nums[i] = 2
+
+        # return nums
+
+        # dutch national flag algo(optimal)
+        n = len(nums)
+
+        low, mid, high = 0, 0, n - 1
+
+        while mid <= high:
+            if nums[mid] == 0:
+                nums[mid], nums[low] = nums[low], nums[mid]
+                mid += 1
+                low += 1
+            
+            elif nums[mid] == 1:
+                mid += 1
+            
             else:
-                cnt2 += 1
+                nums[mid], nums[high] = nums[high], nums[mid]
+                high -= 1
+                
         
-        for i in range(cnt0):
-            nums[i] = 0
-        
-        for i in range(cnt0, cnt0 + cnt1):
-            nums[i] = 1
 
-        for i in range(cnt0 + cnt1, len(nums)):
-            nums[i] = 2
-
-        return nums
