70e57e667866403283773583ca56652b257b4193
Magicisop122 <gdivekar122@gmail.com>
2025-07-13 10:25:49 -0400

    Time: 4 ms (41.84%) | Memory: 19.1 MB (23.75%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -3,14 +3,28 @@ class Solution:
 
         # brute force(n ^ 2)
 
-        res = []
+        # res = []
 
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    res = i, j
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             res = i, j
         
-        return res
+        # return res
+
+        # better using hashmap
+
+        hashmap = {} # key: index, value: number
+
+        for i, n in enumerate(nums):
+            diff = target - n
+
+            if diff in hashmap:
+                return [hashmap[diff], i]
+            
+            hashmap[n] = i
+
+        return []
 
 
         
