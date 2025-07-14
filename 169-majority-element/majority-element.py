9fc4f19f64dddb19075b282ad2fa5b93e5de4607
Magicisop122 <gdivekar122@gmail.com>
2025-07-14 11:52:00 -0400

    Time: 7 ms (57.14%) | Memory: 19.5 MB (19.23%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,17 +1,38 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
 
-        hashmap = {}
-        majority = len(nums) // 2
+        # better approach(extra space)
 
-        for n in nums:
-            if n in hashmap:
-                hashmap[n] += 1
-            else:
-                hashmap[n] = 1
+        # hashmap = {}
+        # majority = len(nums) // 2
+
+        # for n in nums:
+        #     if n in hashmap:
+        #         hashmap[n] += 1
+        #     else:
+        #         hashmap[n] = 1
         
-            if hashmap[n] > majority:
-                return n
+        #     if hashmap[n] > majority:
+        #         return n
+
+        # optimal solution(moore's voting algo)
+
+        ele = nums[0]
+
+        cnt = 1
+
+        for i in range(1, len(nums)):
+            if ele == nums[i]:
+                cnt += 1
+            else:
+                cnt -= 1
+                
+            if cnt == 0:
+                ele = nums[i + 1]
+
+        return ele
+
+
 
         
         
