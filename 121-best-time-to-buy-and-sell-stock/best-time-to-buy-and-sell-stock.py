b17251a78a18d70ef7317ad20d77609c8e5eed94
Magicisop122 <gdivekar122@gmail.com>
2025-07-15 16:46:06 -0400

    Time: 94 ms (50.37%) | Memory: 27 MB (8.36%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,28 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # O(n) solution
 
+        # res = 0
+        # mini = prices[0]
+
+        # for i in range(1, len(prices)):
+        #     profit = prices[i] - mini
+        #     res = max(res, profit)
+        #     mini = min(mini, prices[i])
+        
+        # return res
+        
+        # 2 pointers solution
+        
         res = 0
-        mini = prices[0]
+        l, r = 0, 1
 
-        for i in range(1, len(prices)):
-            profit = prices[i] - mini
-            res = max(res, profit)
-            mini = min(mini, prices[i])
-        
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                res = max(res, profit)
+
+            else:
+                l = r
+            r += 1
         return res
-        
