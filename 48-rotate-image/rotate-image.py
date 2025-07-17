48c8048cc645dd28547a24ed03480d684ed77cb1
Magicisop122 <gdivekar122@gmail.com>
2025-07-17 18:35:06 -0400

    Time: 0 ms (100.00%) | Memory: 18 MB (22.38%) - LeetSync

--- 48-rotate-image/rotate-image.py
+++ 48-rotate-image/rotate-image.py
@@ -5,15 +5,30 @@ class Solution:
         """
         # 0(n * n) solution with extra 0(n * n) memory
 
+        # n = len(matrix)
+        # rotated = [[0] * n for _ in range(n)]
+
+        # for i in range(n):
+        #     for j in range(n):
+        #         rotated[j][n - 1 - i] = matrix[i][j]
+
+        # for i in range(n):
+        #     for j in range(n):
+        #         matrix[i][j] = rotated[i][j]
+
+        # optimal solution without extra memory
+
         n = len(matrix)
-        rotated = [[0] * n for _ in range(n)]
+        
+        # first lets transpose the matrix
 
         for i in range(n):
-            for j in range(n):
-                rotated[j][n - 1 - i] = matrix[i][j]
+            for j in range(i + 1, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # reverse the rows
 
         for i in range(n):
-            for j in range(n):
-                matrix[i][j] = rotated[i][j]
+            matrix[i].reverse()
 
         
