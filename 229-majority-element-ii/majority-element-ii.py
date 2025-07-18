ff30bdd21bd401620ddc86c802c113e5f667e661
Magicisop122 <gdivekar122@gmail.com>
2025-07-18 16:18:10 -0400

    Time: 4 ms (80.55%) | Memory: 20.6 MB (64.23%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.py
+++ 229-majority-element-ii/majority-element-ii.py
@@ -1,21 +1,47 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> List[int]:
+        # extra memory solution hashmap
 
-        
+        # hashmap = {}
+        # final = []
 
-        hashmap = {}
-        final = []
+        # for n in nums:
+        #     if n in hashmap:
+        #         hashmap[n] += 1
+        #     else:
+        #         hashmap[n] = 1
+
+        # for n in hashmap:
+        #     if hashmap[n] > (len(nums) // 3):
+        #         final.append(n)
+
+        # return final
+
+        # boyer moore
+
+        if not nums:
+            return []
+
+        ele1 = ele2 = None
+        cnt1 = cnt2 = 0
 
         for n in nums:
-            if n in hashmap:
-                hashmap[n] += 1
+            if n == ele1:
+                cnt1 += 1
+            elif n == ele2:
+                cnt2 += 1
+            elif cnt1 == 0:
+                ele1, cnt1 = n, 1
+            elif cnt2 == 0:
+                ele2, cnt2 = n, 1
             else:
-                hashmap[n] = 1
-
-        for n in hashmap:
-            if hashmap[n] > (len(nums) // 3):
-                final.append(n)
-
-        return final
-                
+                cnt1 -= 1
+                cnt2 -= 1
         
+        res = []
+
+        for ele in [ele1, ele2]:
+            if nums.count(ele) > len(nums) // 3:
+                res.append(ele)
+
+        return res        
