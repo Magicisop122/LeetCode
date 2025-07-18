0d7927a866c6e4a7401ef16ce24921994cc8e308
Magicisop122 <gdivekar122@gmail.com>
2025-07-18 11:45:02 -0400

    Time: 23 ms (96.32%) | Memory: 20.5 MB (39.24%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -0,0 +1,27 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+        hashmap = {0:1}
+        prefixSum = 0
+        cnt = 0
+
+        for n in nums:
+            prefixSum += n
+
+            if (prefixSum - k) in hashmap:
+                cnt += hashmap[prefixSum - k]
+
+            if prefixSum in hashmap:
+                hashmap[prefixSum] += 1
+            else:
+                hashmap[prefixSum] = 1
+
+        return cnt
+            
+
+       
+
+        
+
+        
+        
