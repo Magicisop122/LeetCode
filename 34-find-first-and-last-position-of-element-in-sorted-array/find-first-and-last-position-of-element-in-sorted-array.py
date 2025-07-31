eb2544119b10a2ab4c1197595f874a691d2b37d0
Magicisop122 <gdivekar122@gmail.com>
2025-07-30 22:32:30 -0400

    Time: 0 ms (100.00%) | Memory: 18.9 MB (93.45%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,12 +1,55 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
 
-        first, last = -1, -1
+        # first, last = -1, -1
 
-        for i in range(len(nums)):
-            if nums[i] == target:
-                if first == -1:
-                    first = i
-                last = i
+        # for i in range(len(nums)):
+        #     if nums[i] == target:
+        #         if first == -1:
+        #             first = i
+        #         last = i
 
-        return [first, last]
+        # return [first, last]
+
+        # binary 
+
+        # [5,7,7,8,8,10]
+        # l = 0 r = 5 mid = 2
+        # l = 3 r = 5 mid = 4 lb = 4 index
+        # l = 5 r = 5 mid = 5
+        
+        lb = -1
+        ub = -1
+
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                lb = mid
+                r = mid - 1
+        
+
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                ub = mid
+                l = mid + 1
+        
+        return [lb, ub]
+
+
+
+        
