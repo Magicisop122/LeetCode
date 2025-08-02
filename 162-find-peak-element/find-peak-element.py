eab4e5562055e5455e04bd502d2c15f79474d1e5
Magicisop122 <gdivekar122@gmail.com>
2025-08-02 11:03:10 -0400

    Time: 0 ms (100.00%) | Memory: 18.1 MB (18.92%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,18 +1,42 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
 
+        # brute force
+
+        # if len(nums) == 1:
+        #     return 0
+
+        # if nums[0] > nums[1]:
+        #     return 0
+
+        # if nums[len(nums) - 1] > nums[len(nums) - 2]:
+        #     return len(nums) - 1
+
+        # for i in range(1, len(nums) - 1):
+        #     if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
+        #         return i
+
+        # binary search
+
+        n = len(nums)
+        if n == 1:
+            return 0
         if nums[0] > nums[1]:
             return 0
-
-        if nums[len(nums) - 1] > nums[len(nums) - 2]:
-            return len(nums) - 1
-
-        for i in range(1, len(nums) - 1):
-            if nums[i] > nums[i - 1] and nums[i] > nums[i + 1]:
-                return i
-
+        if nums[n - 1] > nums[n - 2]:
+            return n - 1
         
+        l, r = 1, n - 2
+
+        while l <= r:
+            mid = l + (r - l) // 2
+
+            if nums[mid - 1] < nums[mid] > nums[mid + 1]:
+                return mid
+            else:
+                if nums[mid] > nums[mid - 1]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
 
         
