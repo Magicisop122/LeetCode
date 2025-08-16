4bd2b4073408231e57d873e0a4f591e1bd452f65
Magicisop122 <gdivekar122@gmail.com>
2025-08-16 12:14:40 -0400

    Time: 150 ms (26.72%) | Memory: 24 MB (46.38%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -15,26 +15,42 @@ class Solution:
 
         # better solution
 
-        def binarySearch(nums, target):
-            n = len(nums)
-            l, r = 0, len(nums) - 1
+        # def binarySearch(nums, target):
+        #     n = len(nums)
+        #     l, r = 0, len(nums) - 1
 
-            while l <= r:
-                mid = l + (r - l) // 2
-                if nums[mid] == target:
-                    return True
-                elif nums[mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
+        #     while l <= r:
+        #         mid = l + (r - l) // 2
+        #         if nums[mid] == target:
+        #             return True
+        #         elif nums[mid] < target:
+        #             l = mid + 1
+        #         else:
+        #             r = mid - 1
 
-            return False
+        #     return False
 
-        m = len(matrix)
+        # m = len(matrix)
         
-        for i in range(m):
-            flag = binarySearch(matrix[i], target)
-            if flag:
-                return True
+        # for i in range(m):
+        #     flag = binarySearch(matrix[i], target)
+        #     if flag:
+        #         return True
             
+        # return False
+
+        # optimal solution
+
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        row, col = 0, COLS - 1
+
+        while row < ROWS and col >= 0:
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] > target:
+                col = col - 1
+            else:
+                row = row + 1
+        
         return False
