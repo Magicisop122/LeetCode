a13061563ec4e9c002b0c4fc0dedfbf0700f33d8
Magicisop122 <gdivekar122@gmail.com>
2025-08-16 12:15:35 -0400

    Time: 143 ms (64.92%) | Memory: 24.1 MB (46.38%) - LeetSync

--- 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.py
@@ -3,41 +3,41 @@ class Solution:
 
         # brute force
 
-        # ROWS, COLS = len(matrix), len(matrix[0])
-        # res = False
+        ROWS, COLS = len(matrix), len(matrix[0])
+        res = False
 
-        # for i in range(ROWS):
-        #     for j in range(COLS):
-        #         if matrix[i][j] == target:
-        #             res = True
+        for i in range(ROWS):
+            for j in range(COLS):
+                if matrix[i][j] == target:
+                    res = True
 
-        # return res
+        return res
 
         # better solution
 
-        # def binarySearch(nums, target):
-        #     n = len(nums)
-        #     l, r = 0, len(nums) - 1
+        def binarySearch(nums, target):
+            n = len(nums)
+            l, r = 0, len(nums) - 1
 
-        #     while l <= r:
-        #         mid = l + (r - l) // 2
-        #         if nums[mid] == target:
-        #             return True
-        #         elif nums[mid] < target:
-        #             l = mid + 1
-        #         else:
-        #             r = mid - 1
+            while l <= r:
+                mid = l + (r - l) // 2
+                if nums[mid] == target:
+                    return True
+                elif nums[mid] < target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
 
-        #     return False
+            return False
 
-        # m = len(matrix)
+        m = len(matrix)
         
-        # for i in range(m):
-        #     flag = binarySearch(matrix[i], target)
-        #     if flag:
-        #         return True
+        for i in range(m):
+            flag = binarySearch(matrix[i], target)
+            if flag:
+                return True
             
-        # return False
+        return False
 
         # optimal solution
 
