836dfe941fac2696133e8a0cc65cbd8d0909a673
Magicisop122 <gdivekar122@gmail.com>
2025-08-19 12:22:21 -0400

    Time: 3 ms (1.58%) | Memory: 17.7 MB (79.58%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -8,25 +8,33 @@ class Solution:
 
         # O(n) solution can call it brute force
 
-        temp = head
-        cnt = 0
-        while temp != None:
-            cnt += 1
-            temp = temp.next
+        # temp = head
+        # cnt = 0
+        # while temp != None:
+        #     cnt += 1
+        #     temp = temp.next
 
-        mid = (cnt // 2) + 1
+        # mid = (cnt // 2) + 1
 
-        temp = head
+        # temp = head
 
-        while temp != None:
-            mid -= 1
-            if mid == 0:
-                break
-            temp = temp.next
+        # while temp != None:
+        #     mid -= 1
+        #     if mid == 0:
+        #         break
+        #     temp = temp.next
 
-        return temp
+        # return temp
 
-        # 
+        # optimal approach (tortoise and hare method)
+
+        slow, fast = head, head
+
+        while (fast != None and fast.next != None):
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
         
 
         
