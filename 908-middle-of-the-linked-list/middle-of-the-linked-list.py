30ff9a616141ef74a52c8b0513be5e59c92fe030
Magicisop122 <gdivekar122@gmail.com>
2025-08-19 12:07:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.6 MB (79.58%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -6,20 +6,26 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
 
+        # O(n) solution can call it brute force
+
         temp = head
         cnt = 0
         while temp != None:
-            temp = temp.next
             cnt += 1
+            temp = temp.next
 
-        mid = (cnt // 2) 
+        mid = (cnt // 2) + 1
 
         temp = head
 
-        for _ in range(mid):
-            temp = temp.next
+        while temp != None:
+            mid -= 1
+            if mid == 0:
+                return temp
+            else:
+                temp = temp.next
 
-        return temp
+        # 
         
 
         
