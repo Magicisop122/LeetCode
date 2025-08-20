f24c627473a691229eee17f6a3dee78ae494b639
Magicisop122 <gdivekar122@gmail.com>
2025-08-20 14:47:08 -0400

    Time: 51 ms (17.14%) | Memory: 34.7 MB (84.34%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -6,20 +6,55 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
 
-        stack = []
-        curr = head
+        # stack method with extra memory 
 
-        while curr:
-            stack.append(curr.val)
-            curr = curr.next
+        # stack = []
+        # curr = head
 
-        curr = head
+        # while curr:
+        #     stack.append(curr.val)
+        #     curr = curr.next
 
-        while stack:
-            if stack.pop() != curr.val:
-                return False
-            curr = curr.next
+        # curr = head
+
+        # while stack:
+        #     if stack.pop() != curr.val:
+        #         return False
+        #     curr = curr.next
         
+        # return True
+
+
+        # optimal 2 pointers
+
+        # lets find middle 
+
+        slow, fast = head, head
+
+        while fast != None and fast.next != None:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse the second half
+
+        prev = None
+
+        while slow != None:
+            temp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = temp
+
+        # compare with the reversed part
+
+        l, r = head, prev
+
+        while r != None:
+            if l.val != r.val:
+                return False
+            l = l.next
+            r = r.next
+
         return True
         
         
