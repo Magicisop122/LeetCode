d81a22505fc01fda2d74a423802ffe697160399b
Magicisop122 <gdivekar122@gmail.com>
2025-09-30 20:10:46 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (59.23%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -8,7 +8,5 @@ class Solution:
             row = []
             for j in range(len(res[-1]) + 1):
                 row.append(temp[j] + temp[j + 1])
-            
             res.append(row)
         return res
-        
