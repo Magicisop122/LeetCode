165c1d6bd0e3bd6f12a5019343b9e95f9dc4d4bd
Magicisop122 <gdivekar122@gmail.com>
2026-01-27 08:37:22 -0500

    Time: 0 ms (100.00%) | Memory: 20.4 MB (22.66%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -4,12 +4,13 @@ class Solution:
         hashmap = {}
 
         for i, n in enumerate(nums):
+
             diff = target - n
             if diff in hashmap:
                 return [hashmap[diff], i]
             
             hashmap[n] = i
+        
 
             
-
         
