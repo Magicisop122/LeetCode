98117428b339b08d8daa0ef13fbe9d5e3567d005
Magicisop122 <gdivekar122@gmail.com>
2026-01-27 09:26:44 -0500

    Time: 11 ms (83.43%) | Memory: 22.2 MB (34.97%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -2,13 +2,13 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         # sorting solution
 
-        # anagrams = defaultdict(list)
+        anagrams = defaultdict(list)
 
-        # for word in strs:
-        #     key = "".join(sorted(word))
-        #     anagrams[key].append(word)
+        for word in strs:
+            key = "".join(sorted(word))
+            anagrams[key].append(word)
 
-        # return list(anagrams.values())
+        return list(anagrams.values())
 
         # hashmap solution
 
