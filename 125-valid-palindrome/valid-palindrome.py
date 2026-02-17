0074fc60467f639ec9f89357e8494404a42ba15e
Magicisop122 <gdivekar122@gmail.com>
2026-02-17 08:57:12 -0500

    Time: 7 ms (82.16%) | Memory: 19.6 MB (72.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,41 +1,10 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # Functional recursion
+        # cheating solution because it uses python in built functions
 
-        # newS = ''.join(c.lower() for c in s if c.isalnum())
+        newString = ""
 
-        # def valid(i):
-        #     if i >= (len(newS) // 2):
-
-        #         return True
-            
-        #     if newS[i] != newS[len(newS) - i - 1]:
-        #         return False
-            
-        #     return valid(i + 1)
-
-        # return valid(0)
-
-
-
-        #  regex method
-
-        # s = s.lower()
-
-        # c = re.sub(r'[^a-z0-9]', '', s)
-
-        # return c == c[::-1]
-
-        # 2 Pointers
-
-        newS = ''.join(c.lower() for c in s if c.isalnum())
-        l, r = 0, len(newS) - 1
-
-        while l <= r:
-            if newS[l] != newS[r]:
-                return False
-            
-            l += 1
-            r -= 1
-        
-        return True
+        for c in s:
+            if c.isalnum():
+                newString += c.lower()
+        return newString == newString[::-1]
