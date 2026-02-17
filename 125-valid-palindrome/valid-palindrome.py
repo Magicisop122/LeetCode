a87c9ed7da4876394ab52f30bf795f120ce98c30
Magicisop122 <gdivekar122@gmail.com>
2026-02-17 09:45:57 -0500

    Time: 23 ms (5.31%) | Memory: 19.6 MB (72.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -2,9 +2,37 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         # cheating solution because it uses python in built functions
 
-        newString = ""
+        # newString = ""
 
-        for c in s:
-            if c.isalnum():
-                newString += c.lower()
-        return newString == newString[::-1]
+        # for c in s:
+        #     if c.isalnum():
+        #         newString += c.lower()
+        # return newString == newString[::-1]
+
+        # optimal way of solving using 2 pointers
+
+        def isAlnum(c):
+            return (ord('A') <= ord(c) <= ord('Z') or
+                    ord('a') <= ord(c) <= ord('z') or
+                    ord('0') <= ord(c) <= ord('9'))
+
+        l, r = 0, len(s) - 1
+
+
+        while l < r:
+            while l < r and not isAlnum(s[l]):
+                l += 1
+            while r > l and not isAlnum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+        
+            l += 1
+            r -= 1
+        
+        return True
+            
+
+
+
+        
