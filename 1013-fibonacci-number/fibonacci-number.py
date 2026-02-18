9c629dc13c3fd4b4a97124f29da3a0a931333edc
Magicisop122 <gdivekar122@gmail.com>
2026-02-18 09:28:02 -0500

    Time: 321 ms (25.17%) | Memory: 19.3 MB (44.66%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -3,31 +3,37 @@ class Solution:
 
         # using basic for loop
 
+        # if n == 0:
+        #     return 0
+        # if n == 1:
+        #     return 1
+        
+        # f = [0] * (n + 1)
+        # f[0] = 0
+        # f[1] = 1
+
+        # for i in range(2, n + 1):
+        #     f[i] = f[i - 1] + f[i - 2]
+
+        # return f[n]
+
+        # # recursion
+
+        # def fibbo(n):
+        #     if n <= 1:
+        #         return n
+
+        #     last = fibbo(n - 1)
+        #     slast = fibbo(n - 2)
+        #     return last + slast
+
+        # return fibbo(n)
         if n == 0:
             return 0
+        
         if n == 1:
             return 1
-        
-        f = [0] * (n + 1)
-        f[0] = 0
-        f[1] = 1
 
-        for i in range(2, n + 1):
-            f[i] = f[i - 1] + f[i - 2]
-
-        return f[n]
-
-        # recursion
-
-        def fibbo(n):
-            if n <= 1:
-                return n
-
-            last = fibbo(n - 1)
-            slast = fibbo(n - 2)
-            return last + slast
-
-        return fibbo(n)
-
+        return self.fib(n - 1) + self.fib(n - 2)
        
         
