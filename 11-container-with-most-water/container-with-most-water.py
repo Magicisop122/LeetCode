8df441c2ae42efeddfa7a6b053458e86072305f2
Magicisop122 <gdivekar122@gmail.com>
2026-03-05 16:39:19 -0500

    Time: 55 ms (73.76%) | Memory: 29.7 MB (33.45%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -1,23 +1,12 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        # brute force which gives TLE
-        # res = 0
-
-        # for l in range(len(height)):
-        #     for r in range(l + 1, len(height)):
-        #         area = (r - l) * min(height[l], height[r])
-        #         res = max(res, area)
-
-        # return res
-        
         res = 0
 
         l, r = 0, len(height) - 1
 
         while l < r:
-            area = (r - l) * min(height[l], height[r])
+            area = min(height[l], height[r]) * (r - l)
             res = max(res, area)
-
             if height[l] < height[r]:
                 l += 1
             elif height[l] > height[r]:
@@ -26,3 +15,4 @@ class Solution:
                 l += 1
         
         return res
+        
