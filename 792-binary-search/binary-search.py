e6770e053517757bbfd8bec3d74fd1a64b837e22
Magicisop122 <gdivekar122@gmail.com>
2026-03-06 21:53:04 -0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (72.11%) - LeetSync

--- 792-binary-search/binary-search.py
+++ 792-binary-search/binary-search.py
@@ -3,32 +3,15 @@ class Solution:
 
         l, r = 0, len(nums) - 1
 
-        for i in range(len(nums)):
-            mid = (l + r) // 2
+        while l <= r:
+            mid = l + (r - l) // 2
 
-            if target < nums[mid]:
-                r = mid - 1
-            elif target > nums[mid]:
+            if nums[mid] < target:
                 l = mid + 1
+            elif nums[mid] > target:
+                r = mid - 1
             else:
                 return mid
 
         return -1
         
-
-        # recursive
-
-        def helper(left, right):
-            if left > right:
-                return -1
-
-            mid = (left + right) // 2
-
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                return helper(mid + 1, right)
-            else:
-                return helper(left, mid - 1)
-        return helper(0, len(nums) - 1)
-    
