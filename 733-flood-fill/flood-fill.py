48a7f42e67c2300a241a91212d77e59a77f68962
Magicisop122 <gdivekar122@gmail.com>
2026-03-10 13:26:50 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (81.60%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -2,27 +2,33 @@ class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
 
         ROWS, COLS = len(image), len(image[0])
-        original = image[sr][sc]
+        visit = set()
+        q = deque()
+        startColor = image[sr][sc]
 
-        if original == color:
+        if startColor == color:
             return image
+        
+        q.append((sr, sc))
+        visit.add((sr, sc))
 
-        def dfs(r, c):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS):
-                return
-            if image[r][c] != original:
-                return
-            
+        while q:
+            r, c = q.popleft()
             image[r][c] = color
 
-            dfs(r + 1, c)
-            dfs(r - 1, c)
-            dfs(r, c + 1)
-            dfs(r, c - 1)
+            neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]]
 
-        dfs(sr, sc)
-
+            for dr, dc in neighbors:
+                if ((r + dr) < 0 or (c + dc) < 0 or
+                    r + dr == ROWS or c + dc == COLS or
+                    (r + dr, c + dc) in visit or
+                    image[r + dr][c + dc] != startColor):
+                    continue
+                q.append((r + dr, c + dc))
+                visit.add((r + dr, c + dc))
+            
         return image
+
       
         
 
